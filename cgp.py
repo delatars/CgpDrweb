2c49ec9ac844ea6ccb548cd3b3c79701e3f6e6e2
Alexandr Morokov <a.morokov@drweb.com>
2019-04-19 21:11:51 +0300

    added spamc report support

--- cgp.py
+++ cgp.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import re
 from subprocess import PIPE, Popen
 from random import randint
 
@@ -14,13 +15,13 @@ SPAMD_PORT = "<SPAMD_PORT>"
 ####### SERVICE VARS ########
 DEBUG = False
 DEBUG_LOGDIR = CGP_PATH
-SPAMD_CONNECTION_TIMEOUT = 5
+SPAMD_CONNECTION_TIMEOUT = 20
 ###################################################################################
 
 class Devnull:
     def write(self, msg): pass
     def flush(self): pass
-    def __call__(self, a,b): pass
+    def __call__(self, a, b): pass
 
 def logging(logfile, msg):
     f = open(os.path.join(DEBUG_LOGDIR, logfile), "a")
@@ -33,54 +34,53 @@ if DEBUG:
 else:
     DEBUGSTREAM = Devnull()
 
-def parse_message(message):
+
+def split_message(message):
     msg = message.read()
     split_index = msg.find("\n\n")
     headers = msg[: split_index]
-    body = msg[split_index+2:]
-    msg_for_check = "\n"
-    for header in headers:
-        msg_for_check += header
-    msg_for_check += "\n"
-    for line in body:
-        msg_for_check += line
-    return msg_for_check, headers, body
+    body = msg[split_index:]
+    return headers, body
+
 
 def SpamCheck(message):
-    spamc = Popen("%s -d %s -p %s -t %s -c" % (SPAMC_BIN, SPAMD_SERVER, SPAMD_PORT, SPAMD_CONNECTION_TIMEOUT),
-                        shell=True, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+    """ Function do request to spamd server via spamc utility and return
+         - spamd_connection
+         - score
+         - threshold
+         - report
+    """
+    spamc = Popen([SPAMC_BIN, "-d", SPAMD_SERVER, "-p", str(SPAMD_PORT), "-t", str(SPAMD_CONNECTION_TIMEOUT), "-R"],
+                  stdin=PIPE, stderr=PIPE, stdout=PIPE)
     spamc.stdin.write(bytes(message))
-    result = spamc.communicate()[0].split("/")
-    if result[0] == "0":
-        spamd_connection = False
-        spam_score = "None"
-        spam_result = "None"
-    else:
-        spamd_connection = True
-        spam_score = result[0].strip()
-        if result[1].strip() == "0.0":
-            spam_result = "SPAM"
-        else:
-            spam_result = "NOT_SPAM"
-    return spamd_connection, spam_score, spam_result
+    spamd_connection = "True"
+    result = spamc.communicate()[0]
+    score = re.findall(r"^([\d.]+)/[\d.]+", result)[0]
+    threshold = re.findall(r"^[\d.]+/([\d.]+)", result)[0]
+    report = "\n".join(re.findall(r"\n(.*)", result))
+    if score == "0" and threshold == "0":
+        spamd_connection = "False"
+        score = "none"
+        threshold = "none"
+        report = "none"
+    return spamd_connection, score, threshold, report
 
-def creating_filtered_msg(headers, body, spamd_connection, score, spam_status):
-        filtered_message = "filtered_message_%s.tmp" % str(randint(1, 10000000))
-        with open(os.path.join(CGP_IPC_DIR, filtered_message), "w") as tmp:
-            # adding base headers
-            for line in headers:
-                tmp.write(line)
-            # adding additional headers
-            tmp.write("\n")
-            tmp.write("X-Spamd-Connection: %s\n" % spamd_connection)
-            tmp.write("X-Spam-Status: %s\n" % spam_status)
-            tmp.write("X-Spam-Score: %s\n" % score)
-            # adding headers delimiter
-            tmp.write("\n")
-            # adding body
-            for line in body:
-                tmp.write(line)
-        return filtered_message
+
+def creating_filtered_msg(headers, body, spamd_connection, score, threshold, report):
+    filtered_message = "filtered_message_%s.tmp" % str(randint(1, 10000000))
+    with open(os.path.join(CGP_IPC_DIR, filtered_message), "w") as tmp:
+        tmp.write(headers)
+        tmp.write("\n")
+        tmp.write("X-Spamd-Connection: %s" % spamd_connection)
+        tmp.write("\n")
+        tmp.write("X-Spam-Score: %s" % score)
+        tmp.write("\n")
+        tmp.write("X-Spam-Threshold: %s" % threshold)
+        tmp.write("\n")
+        tmp.write("X-Spam-Report: %s" % report)
+        tmp.write(body)
+    return filtered_message
+
 
 def proceed(message):
     if DEBUG:
@@ -94,11 +94,10 @@ if __name__ == "__main__":
     # Getting fileobject from stdin
     message = sys.stdin
     # Split message and removing unused info
-    msg_for_check, headers, body = parse_message(message)
-    print >> DEBUGSTREAM("stdin.log", msg_for_check)
+    headers, body = split_message(message)
     # Send message for audit to Spamd
-    spamd_connection, score, spam_status = SpamCheck(msg_for_check)
+    spamd_connection, score, treshold, report = SpamCheck(message)
     # Adding additional headers to message
-    filtered_msg = creating_filtered_msg(headers, body, spamd_connection, score, spam_status)
+    filtered_msg = creating_filtered_msg(headers, body, spamd_connection, score, treshold, report)
     # Proceed to CGP Queue
     proceed(filtered_msg)
